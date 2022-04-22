daac93d457924b289a06cef363852cb85bfe66b7
Sabbir Ahmed <sabbir1cse@outlook.com>
2022-04-22 16:32:30 +0600

    # Fix   - Tree view
    TODO:
     Bug fix, Security

--- app.py
+++ app.py
@@ -1,42 +1,86 @@
 import mistletoe
 import os
-from flask import Flask, render_template
-
-app = Flask(__name__)
+from flask import Flask, render_template, request, abort
 
 DOC_DIRECTORY = f'{os.getcwd()}{os.sep}doc'
-MARKDOWN_LIST = []
-GENERATED = False
 
 
-def generate_link(file_with_path, file):
-    current_path = file_with_path.replace(DOC_DIRECTORY, "").split(os.sep)
-    link = dict(name=file, path=file_with_path.replace(DOC_DIRECTORY, ""), depth=current_path)
-    MARKDOWN_LIST.append(link)
+def directory_tree(path):
+    string_build = ""
+    space = "&nbsp;&nbsp;&nbsp;"
+    first = True
+    for dirpath, dirnames, filenames in os.walk(path):
+        directory_level = dirpath.replace(path, "")
+        directory_level = directory_level.count(os.sep)
+        if first:
+            root_display = 'style="display: inline!important"'
+            alter_display = 'style="display: none!important"'
+            first = False
+
+            dir_name = f"{DOC_DIRECTORY.split(os.sep)[-2]}"
+            dir_sanitized = ""
+            dir_id = f"{dir_name}_"
+        else:
+            root_display = ""
+            alter_display = ''
+
+            dir_name = os.path.basename(dirpath)
+            dir_sanitized = dirpath.replace(DOC_DIRECTORY, "")
+            dir_id = dir_sanitized.replace(os.sep, "_")
+
+        indent = space * (4 * directory_level)
+        indent_2 = space * (4 * directory_level + 1)
+
+        string_build += f'''            
+            <div data-id="{dir_id}" class="directory-wrapper">
+                {indent}
+                <span class="directory-close" {root_display}>📁&nbsp;</span>
+                <span class="directory-open" {alter_display}>📂&nbsp;</span>
+                <strong>{dir_name}</strong>/            
+            <div id="{dir_id}" class="directory-child" {root_display}>
+        '''
+
+        for f in filenames:
+            # print(f)
+            string_build += f'''
+            <div>
+                &nbsp;&nbsp;{indent_2}
+                <a class="section-link" href="?dir={dir_sanitized}&file={f}">
+                    <span>🗎&nbsp;</span>{f}
+                </a>            
+            </div>'''
+
+        string_build += "</div></div>"
+
+    return f"<div>{string_build}</div>"
 
 
-def read_directory():
-    global GENERATED
-    if GENERATED is False:
-        for root, dirs, files in os.walk(DOC_DIRECTORY):
-            for file in files:
-                if file.lower().endswith(".md"):
-                    # print(os.path.join(root, file))
-                    generate_link(os.path.join(root, file), file)
-        GENERATED = True
+MARKDOWN_LIST = directory_tree(DOC_DIRECTORY)
 
-
-# GENERATE FILE
-# read_directory()
+app = Flask(__name__)
 
 
 @app.route('/')
 def index():
-    print(MARKDOWN_LIST)
-    r = 'Nothing to render.'
-    with open('./README.md', 'r') as f:
-        r = mistletoe.markdown(f)
-        print(r)
+    directory = request.args.get("dir", "")
+    file = request.args.get("file", "")
+
+    if directory.find("..") != -1:
+        return abort(403, "This request not allowed.")
+    if directory == "":
+        directory = DOC_DIRECTORY
+    else:
+        directory = f"{DOC_DIRECTORY}{directory}"
+    if file == "":
+        file = "README.md"
+
+    try:
+        with open(f'{directory}{os.sep}{file}', 'r') as f:
+            r = mistletoe.markdown(f)
+    except FileNotFoundError:
+        r = 'Please select file from left side tree.'
+        print(f"{directory}{file}")
+
     return render_template('index.html', data=r, markdown_list=MARKDOWN_LIST)
 
 
